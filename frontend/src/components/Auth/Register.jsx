6ba2e576877fb1849439c1aff3bf026a5caec482
pavanpappala32 <pavanpappala32@gmail.com>
2025-12-05 12:19:34 +0530

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/register",
+        "https://careerconnect-backend-0wr3.onrender.com/api/v1/user/register",
         { name, phone, email, role, password },
         {
           headers: {
