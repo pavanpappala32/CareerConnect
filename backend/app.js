80515e2761517b7254d8fcb642b795bf89e3c97b
pavanpappala32 <pavanpappala32@gmail.com>
2025-12-05 13:14:55 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["https://careerconnect-6gf9.onrender.com"],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
