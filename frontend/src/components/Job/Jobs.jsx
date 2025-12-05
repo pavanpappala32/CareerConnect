46d98064a17e0a3fbfa407f5f1a8ba53016389f3
pavanpappala32 <pavanpappala32@gmail.com>
2025-12-05 12:23:09 +0530

    Update Jobs.jsx

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -10,7 +10,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://localhost:4000/api/v1/job/getall", {
+        .get("https://careerconnect-backend-0wr3.onrender.com/api/v1/job/getall", {
           withCredentials: true,
         })
         .then((res) => {
